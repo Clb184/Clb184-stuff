ef48d8f7cc916857690be68e25d1926fd28017b4
Columbio184 <84605945+Clb184@users.noreply.github.com>
2021-05-22 14:54:28 -0500

    Add files via upload

---
+++ PlayableHatate/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "PlayableHatate",
+	"servers": [
+		"https://raw.githubusercontent.com/Clb184/Clb184-stuff/master/PlayableHatate"
+	],
+	"title": "TH09. Replaces Aya A sprites with Hatate sprites",
+	"update": true
+}
