4a5e97800e50b08a3b4f652745c0ff73f6bee376
Clb184 <84605945+Clb184@users.noreply.github.com>
2021-05-22 14:32:39 -0500

    Add files via upload

---
+++ repo.js
@@ -0,0 +1,10 @@
+{
+	"contact": "Clb184#6685",
+    "id": "Clb184",
+    "patches": {
+		"PlayableHatate": "TH09. Replaces Aya A sprites with Hatate sprites"	
+    },
+    "servers":[
+        ""
+    ],
+    "title": "Clb184 stuff"
