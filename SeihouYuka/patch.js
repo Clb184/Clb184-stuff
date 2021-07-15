92a4e452e84a0710a923228c0dfeee536022b99f
Columbio184 <84605945+Clb184@users.noreply.github.com>
2021-07-14 20:29:05 -0500

    Add files via upload

---
+++ SeihouYuka/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "SeihouYuka",
+	"servers": [
+		"https://raw.githubusercontent.com/Clb184/Clb184-stuff/master/SeihouYuka/"
+	],
+	"title": "TH09. Replaces Aya A sprites with Hatate sprites",
+	"update": true
+}
