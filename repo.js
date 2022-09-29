73921dd38f25ae943baa7d200deda670f4da3953
Clb184 <84605945+Clb184@users.noreply.github.com>
2022-09-28 20:09:57 -0500

    Add files via upload

--- repo.js
+++ repo.js
@@ -1,13 +1,13 @@
 {
-    "contact": "Discord @Clb184#6685",
-    "id": "Clb184",
-    "title": "Clb184 stuff",
-    "patches": {
-	"PlayableHatate": "TH09. Replaces Aya A sprites with Hatate sprites."
-        "ctk-731-midi": "TH06-(07-08-09). Replaces the BGMs with recordings of MIDIs of the games played on my Casio CTK-731."
-    },
-    "servers": [
-        "github.com/Clb184"
-    ],
-    "url_desc": "https://github.com/Clb184/Clb184-stuff"
+	"contact": "Discord @Clb184#6685",
+	"id": "Clb184",
+	"patches": {
+		"PlayableHatate": "TH09. Replaces Aya A sprites with Hatate sprites",
+		"ctk-731-midi": "TH06-(07-08-09). Replaces the BGMs with recordings of MIDIs of the games played on my Casio CTK-731."
+	},
+	"servers": [
+		"github.com/Clb184"
+	],
+	"title": "Clb184 stuff",
+	"url_desc": "https://github.com/Clb184/Clb184-stuff"
 }
