be9043e3023527522e463febd542b95b61419e94
Clb184 <84605945+Clb184@users.noreply.github.com>
2022-09-29 18:43:30 -0500

    Add files via upload

--- PlayableHatate/patch.js
+++ PlayableHatate/patch.js
@@ -4,7 +4,8 @@
 	],
 	"id": "PlayableHatate",
 	"servers": [
-		"https://mirrors.thpatch.net/Clb184/PlayableHatate/"
+		"https://mirrors.thpatch.net/Clb184/PlayableHatate/",
+		"https://raw.githubusercontent.com/Clb184/Clb184-stuff/main/PlayableHatate/"
 	],
 	"title": "TH09. Replaces Aya A sprites with Hatate sprites",
 	"update": true
