4e728a0575569969fcd9d3b90b98886d0ce5d7b7
Clb184 <84605945+Clb184@users.noreply.github.com>
2022-09-29 18:43:01 -0500

    Add files via upload

--- repo.js
+++ repo.js
@@ -6,7 +6,8 @@
 		"ctk-731-midi": "TH06-(07-08-09). Replaces the BGMs with recordings of MIDIs of the games played on my Casio CTK-731."
 	},
 	"servers": [
-		"https://mirrors.thpatch.net/Clb184/"
+		"https://mirrors.thpatch.net/Clb184/",
+		"https://raw.githubusercontent.com/Clb184/Clb184-stuff/main/"
 	],
 	"title": "Clb184 stuff"
 }
