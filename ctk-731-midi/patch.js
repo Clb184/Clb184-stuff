8e8df92945f7084aed9e6155ee41badf277e3a5a
Clb184 <84605945+Clb184@users.noreply.github.com>
2022-09-28 20:45:26 -0500

    Add files via upload

--- ctk-731-midi/patch.js
+++ ctk-731-midi/patch.js
@@ -2,9 +2,9 @@
 	"dependencies": [
 		"nmlgc/base_tsa"
 	],
-	"id": "CTK-731",
+	"id": "ctk-731-midi",
 	"servers": [
-		"https://raw.githubusercontent.com/Clb184/Clb184-stuff/master/ctk-731-midi/"
+		"github.com/Clb184/ctk-731-midi/"
 	],
 	"title": "TH06-(07-08-09). Replaces the BGMs with recordings of MIDIs of the games played on my Casio CTK-731.",
 	"update": true
