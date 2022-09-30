b154ff2047f4b758fca1b0ff1c3ce2d430d17982
Clb184 <84605945+Clb184@users.noreply.github.com>
2022-09-30 14:18:54 -0500

    3rd part of the th07 midi upload, Done!

--- repo.js
+++ repo.js
@@ -4,7 +4,7 @@
 	"patches": {
 		"PlayableHatate": "TH09. Replaces Aya A sprites with Hatate sprites",
 		"SeihouYuka": "TH09. Replaces Yuuka sprites with her Kioh Gyoku sprites.",
-		"ctk-731-midi": "TH06-(07-08-09). Replaces the BGMs with recordings of MIDIs of the games played on my Casio CTK-731."
+		"ctk-731-midi": "TH06-07-(08-09). Replaces the BGMs with recordings of MIDIs of the games played on my Casio CTK-731."
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/Clb184/",
