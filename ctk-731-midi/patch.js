602c407191b51b83e261a12fb6cd0ee7e0e6a438
Clb184 <84605945+Clb184@users.noreply.github.com>
2022-09-30 14:18:36 -0500

    3rd part of the th07 midi upload, Done!

--- ctk-731-midi/patch.js
+++ ctk-731-midi/patch.js
@@ -7,6 +7,6 @@
 		"https://mirrors.thpatch.net/Clb184/ctk-731-midi/",
 		"https://raw.githubusercontent.com/Clb184/Clb184-stuff/main/ctk-731-midi/"
 	],
-	"title": "TH06-(07-08-09). Replaces the BGMs with recordings of MIDIs of the games played on my Casio CTK-731.",
+	"title": "TH06-07-(08-09). Replaces the BGMs with recordings of MIDIs of the games played on my Casio CTK-731.",
 	"update": true
 }
