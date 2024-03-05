d5cdde00c991c5f7bbd68474898f6f90fdafcdd2
Clb184 <84605945+Clb184@users.noreply.github.com>
2024-03-05 12:21:50 -0500

    Upload an old mod (begin)

---
+++ patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "TeamTouhou2",
+	"servers": [
+		"https://mirrors.thpatch.net/Clb184/TeamTouhou2/",
+		"https://raw.githubusercontent.com/Clb184/Clb184-stuff/main/TeamTouhou2/"
+	],
+	"title": "TH06. Replaces different portraits with the mercs from TF2. This was my first mod (2020).",
+	"update": true
+}
